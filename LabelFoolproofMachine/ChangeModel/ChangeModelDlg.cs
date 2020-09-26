931b62482c34ce84d5084703bba14890fecf644d
LLLLLLLLLLLLLLJ <57827402+LLLLLLLLLLLLLLJ@users.noreply.github.com>
2020-09-26 15:09:27 +0800

    tt gfd

--- LabelFoolproofMachine/ChangeModel/ChangeModelDlg.cs
+++ LabelFoolproofMachine/ChangeModel/ChangeModelDlg.cs
@@ -21,5 +21,10 @@ namespace LabelFoolproofMachine
         {
             this.Close();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ///dsadasdsa
+        }
     }
 }
